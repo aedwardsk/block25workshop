6ec5869db056b8ce56641ef1bd45fd418d05e4d7
Andy Edwards <andyedwards@outlook.com>
2025-02-03 09:40:04 -0500

    figured out boarder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+
 // Write your Color component here
+const Color = ({ color, selectedColor, setSelectedColor }) => {
+  const selected = `${color} ${selectedColor === color ? "selected" : ""}`;
+  return <div className={selected} onClick={() => setSelectedColor(color)}></div>;
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState(" ");
   return (
-    <div id="container">
-      <div id="navbar">
+    <div id='container'>
+      <div id='navbar'>
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id='colors-list'>
+        <Color color='green' selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color='violet' selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color='orange' selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
